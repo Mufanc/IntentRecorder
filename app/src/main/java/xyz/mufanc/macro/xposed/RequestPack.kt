1f856aa5b4741adf0fe0176603a0acc7b4f18baf
Mufanc <mufanc.xyz@gmail.com>
2022-09-12 17:44:26 +0800

    refactor: don't clone intent

--- app/src/main/java/xyz/mufanc/macro/xposed/RequestPack.kt
+++ app/src/main/java/xyz/mufanc/macro/xposed/RequestPack.kt
@@ -10,8 +10,9 @@ class RequestPack(holder: Bundle) : BundleWrapper(holder) {
     val callingPackage: String by holder
 
     constructor(request: Any) : this(Bundle()) {
-        val intent = request.getFieldAs<Intent>("intent")!!.clone() as Intent
-        holder.putParcelable(::intent.name, intent)
+//        val intent = request.getFieldAs<Intent>("intent")!!.clone() as Intent
+//        holder.putParcelable(::intent.name, intent)
+        holder.putParcelable(::intent.name, request.getFieldAs(::intent.name))
         holder.putString(::callingPackage.name, request.getFieldAs(::callingPackage.name))
     }
 }
